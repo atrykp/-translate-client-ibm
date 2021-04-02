b6c93f13efda68082f510746fb6c59e657631b47
Patryk Plaza <atrykp0@gmail.com>
2021-04-02 07:02:59 +0200

    add style to TranslationList

--- src/Views/ListPage/ListPage.js
+++ src/Views/ListPage/ListPage.js
@@ -1,11 +1,27 @@
-import { useSelector } from "react-redux";
+import styled from "styled-components";
+
 import TranslationList from "../../components/Organisms/TranslationList/TranslationList";
+import MainTemplate from "../../templates/MainTemplate";
+const StyledBottonBarWrapper = styled.div`
+  position: fixed;
+  bottom: 0;
+  left: 50%;
+  transform: translateX(-50%);
+  align-self: end;
+  width: 100%;
+  max-width: 600px;
 
+  margin: 0 auto;
+`;
 const ListPage = () => {
-  const translationList = useSelector((state) => state.listReducer);
-  const arr = translationList.map((element) => element.translation);
-
-  return <>{<TranslationList></TranslationList>}</>;
+  return (
+    <>
+      <TranslationList></TranslationList>
+      <StyledBottonBarWrapper>
+        <MainTemplate />
+      </StyledBottonBarWrapper>
+    </>
+  );
 };
 
 export default ListPage;
--- src/components/Organisms/TranslationList/TranslationList.js
+++ src/components/Organisms/TranslationList/TranslationList.js
@@ -1,10 +1,23 @@
 import styled from "styled-components";
 import TranslationElement from "../../Molecules/TranslationElement/TranslationElement";
+import { useSelector } from "react-redux";
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  max-width: 600px;
+  margin: 10px auto;
+`;
 
 const TranslationList = () => {
+  const translationList = useSelector((state) => state.listReducer);
+  const arr = translationList.map((element) => element.translation);
+
   return (
     <>
-      <TranslationElement></TranslationElement>
+      <StyledWrapper>
+        <TranslationElement></TranslationElement>
+      </StyledWrapper>
     </>
   );
 };
