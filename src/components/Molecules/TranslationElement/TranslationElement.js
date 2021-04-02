c37d97f270a7c436a438eca742c097371c20cf8f
Patryk Plaza <atrykp0@gmail.com>
2021-04-02 05:57:45 +0200

    add TranslationElement and TranslationList files

---
+++ src/components/Molecules/TranslationElement/TranslationElement.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const TranslationElement = () => {
+  return (
+    <>
+      <h1>element listy</h1>
+    </>
+  );
+};
+
+export default TranslationElement;
---
+++ src/components/Organisms/TranslationList/TranslationList.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+const TranslationList = () => {
+  return (
+    <>
+      <h1>TranslationList</h1>
+    </>
+  );
+};
+
+export default TranslationList;
