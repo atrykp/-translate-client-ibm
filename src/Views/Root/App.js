440a2e659cdb88bb0ee46a107e208a0b30d80d69
Patryk Plaza <atrykp0@gmail.com>
2021-06-13 06:22:25 +0200

    add login and register screen

--- src/Views/Root/App.js
+++ src/Views/Root/App.js
@@ -1,10 +1,12 @@
 import GlobalStyle from "../../Theme/GlobalStyles";
 import { ThemeProvider } from "styled-components";
 import theme from "../../Theme/MainTheme";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Redirect, Route } from "react-router-dom";
 import MainPage from "../MainPage/MainPage";
 import ListPage from "../ListPage/ListPage";
 import FlashCards from "../FlashCards/FlashCards";
+import UserLogin from "../UserLogin/UserLogin";
+import UserRegister from "../UserRegister/UserRegister";
 
 function App() {
   return (
@@ -14,6 +16,9 @@ function App() {
         <Route exact path="/" component={MainPage} />
         <Route path="/list" component={ListPage} />
         <Route path="/flashcards" component={FlashCards} />
+        <Redirect path="/" />
+        <Route path="/register" component={UserRegister} />
+        <Route path="/login" component={UserLogin} />
       </ThemeProvider>
     </Router>
   );
---
+++ src/Views/UserLogin/UserLogin.js
@@ -0,0 +1,71 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+import Input from "../../components/Atoms/Input/Input";
+import Paragraph from "../../components/Atoms/Paragraph/Paragraph";
+import Button from "../../components/Atoms/Button/Button";
+
+const StyledWrapper = styled.div`
+  background-color: ${({ theme }) => theme.colors.lightBackground};
+  height: 100vh;
+  width: 100%;
+  display: grid;
+`;
+const StyledInput = styled(Input)`
+  width: 90%;
+  margin: 10px auto;
+`;
+const StyledFormWrapper = styled.div`
+  justify-self: center;
+  align-self: center;
+  display: grid;
+  width: 95%;
+  max-width: 400px;
+  padding: 50px 0;
+  border-radius: 10px;
+  background-color: lightgray;
+`;
+const StyledParagraph = styled(Paragraph)`
+  position: absolute;
+  font-size: ${({ theme }) => theme.fontSize.medium};
+  color: ${({ theme }) => theme.colors.secondaryDark};
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -1000%);
+`;
+const StyledLabelParagraph = styled(Paragraph)`
+  margin-left: 5%;
+  margin-top: 2%;
+`;
+const StyledButtonsWrapper = styled.div`
+  display: grid;
+  justify-self: center;
+  justify-content: space-between;
+`;
+const StyledLink = styled(Link)`
+  text-align: center;
+  margin-top: 20px;
+`;
+const StyledButton = styled(Button)`
+  margin-top: 10px;
+`;
+
+const UserLogin = () => {
+  return (
+    <StyledWrapper>
+      <StyledParagraph>Login</StyledParagraph>
+      <StyledFormWrapper>
+        <StyledLabelParagraph>Email:</StyledLabelParagraph>
+        <StyledInput type="email" />
+        <StyledLabelParagraph>Password:</StyledLabelParagraph>
+        <StyledInput type="password" />
+        <StyledButtonsWrapper>
+          <StyledButton>Login</StyledButton>
+          <StyledLink to="/register">Sign Up</StyledLink>
+        </StyledButtonsWrapper>
+      </StyledFormWrapper>
+    </StyledWrapper>
+  );
+};
+
+export default UserLogin;
